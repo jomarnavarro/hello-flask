6ea68384805551a3e59175c26b34d030c4bb0c1f
Omar Navarro <jomarnavarro@gmail.com>
2021-08-20 20:37:11 -0500

    Adds pagination for posts

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['user@example.com']
+    POSTS_PER_PAGE = 10
